4e043e42779274b563aced63733cb695ace91e76
Ellen O'Connor <39439718-eoconnor9484@users.noreply.replit.com>
2025-02-15 22:36:19 +0000

    Assistant checkpoint: Added scrolling to create listing dialog
    Assistant generated file changes:
    - client/src/components/create-listing-dialog.tsx: Make dialog content scrollable
    
    ---
    
    User prompt:
    
    Make the create listing dialog scrollable

--- client/src/components/create-listing-dialog.tsx
+++ client/src/components/create-listing-dialog.tsx
@@ -86,7 +86,7 @@ export default function CreateListingDialog() {
       <DialogTrigger asChild>
         <Button>Create Listing</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[425px] max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>Create New Listing</DialogTitle>
           <DialogDescription>
