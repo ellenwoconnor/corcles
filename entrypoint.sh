167ddb0406cf047d9f0f80d86ebb8843e859f6f2
moberries <moberries@moberriess-mbp.lan>
2025-03-15 14:24:07 -0700

    Inject build time secret

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Only write the CA cert if the environment variable is set
-if [ -n "$PG_CA_CERT" ]; then
-    echo "$PG_CA_CERT" > /app/ca-certificate.crt
-    export SSLROOTCERT=/app/ca-certificate.crt
-fi
-
-# Run the main container process
-exec "$@"
