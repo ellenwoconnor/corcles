388227e7cee862437d4edb2a8be8ee70f2b23e0b
eoconnor9484 <39439718-eoconnor9484@users.noreply.replit.com>
2025-05-14 03:54:58 +0000

    Restored to '69b9b38bac27e2cddd9b66028e7c5f355696cdf9'
    Replit-Restored-To: 69b9b38bac27e2cddd9b66028e7c5f355696cdf9

--- client/src/components/community-invitations/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as CommunityInvitationManager } from './CommunityInvitationManager';
-export { default as InviteLinkSection } from './components/InviteLinkSection';
-export { default as SentInvitesList } from './components/SentInvitesList';
-export { default as EmailInviteDialog } from './components/EmailInviteDialog';
-export * from './hooks/useInvitations';
